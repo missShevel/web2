62594aa2760b655c94b41d67ce9c5641faed87ad
Olha <o.v.shevel@gmail.com>
2021-10-30 14:26:38 +0300

    config4

--- api/mailsender.js
+++ api/mailsender.js
@@ -1,14 +1,14 @@
-import { createTransport } from "nodemailer";
-import sanitizeHTML from "sanitize-html";
+import { createTransport } from 'nodemailer';
+import sanitizeHTML from 'sanitize-html';
 //do we need it??
-require("dotenv").config();
+require('dotenv').config();
 
 const from = `Form - ${process.env.EMAIL_ADDRESS}`;
 const history = new Map();
 
 function getTransporter() {
   return createTransport({
-    host: "smtp.gmail.com",
+    host: 'smtp.gmail.com',
     port: 587,
     secure: false,
     auth: {
@@ -30,32 +30,31 @@ async function sendMail(options) {
 
 async function formSubmit(formData) {
   const textMessage =
-    "You have new form submission. <br>" +
-    "Name:" +
+    'You have new form submission. <br>' +
+    'Name:' +
     formData.name +
-    "<br>" +
-    "Email: " +
+    '<br>' +
+    'Email: ' +
     formData.email +
-    "</br>" +
-    "Message: " +
+    '</br>' +
+    'Message: ' +
     formData.message;
   return sendMail({
     from,
     to: process.env.EMAIL_TO_USER,
-    subject: "New form submission",
+    subject: 'New form submission',
     html: sanitizeHTML(textMessage),
   });
 }
 
 const rateLimit = (ip, limit) => {
-    if (!history.has(ip)) {
-        history.set(ip,1);
-    }
-    if (history.get(ip) > limit) {
-        throw new Error();
-    }
-    history.set(ip, history.get(ip) + 1);
-
+  if (!history.has(ip)) {
+    history.set(ip, 1);
+  }
+  if (history.get(ip) > limit) {
+    throw new Error();
+  }
+  history.set(ip, history.get(ip) + 1);
 };
 
 function validate(formdata) {
@@ -74,29 +73,27 @@ function validate(formdata) {
 }
 
 module.exports = async (req, res) => {
-    console.log(req.body);
-    try {
-        
-        rateLimit(req.headers['x-real-ip'], 3);
-    } catch (e) {
-        return res.status(429).json({
-            errors: ["Limitation error"],
-            result: {
-                success: false,
-            }
-        });
-        
-    }
-    try {
-        validate(req.body);
-      } catch (e) {
-        return res.status(402).json({
-        errors: ["Validation error"],
-        result: {
-            success: false,
-        }
-        });
-     }
-    const result = await formSubmit(req.body);
+  console.log(req.body);
+  try {
+    rateLimit(req.headers['x-real-ip'], 3);
+  } catch (e) {
+    return res.status(429).json({
+      errors: ['Limitation error'],
+      result: {
+        success: false,
+      },
+    });
+  }
+  try {
+    validate(req.body);
+  } catch (e) {
+    return res.status(402).json({
+      errors: ['Validation error'],
+      result: {
+        success: false,
+      },
+    });
+  }
+  const result = await formSubmit(req.body);
   return res.json({ result });
 };
